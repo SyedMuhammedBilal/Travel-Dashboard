6827f7eca927081bffa6b4f4ad2e2a50ee9cfebe
Syed Muhammed Bilal <SyedMuhammedBilal@vtechs-MacBook-Pro.local>
2021-07-25 12:25:55 +0500

    dashboard header

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { useStyles } from "./stylesheet";
+import SearchIcon from '@material-ui/icons/Search';
+import BellIcon from '../../assests/icon/bell.png'
 
 const Dashboard = () => {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
-    <Box className={classes.dashbaordContainer}></Box>
+    <Box className={classes.dashbaordContainer}>
+      <Box className={classes.dashboardInnerContainer}>
+        <Box className={classes.dashboardUserStuffs}>
+          <Box className={classes.dashboardUserGreet}>
+            <Box>
+              <Typography className={classes.dahsboardUserGreeting}>Hello, Bilal! 👋🏻</Typography>
+            </Box>
+            <Box>
+                <Typography className={classes.dashboardWelcomeGreeting}> Welcome back, and explore the world. </Typography>
+            </Box>
+          </Box>
+          <Box className={classes.dashboardSearchItems}>
+            <Box>
+                <Box className={classes.dashboardDestinationSearchBox}>
+                    <SearchIcon className={classes.searchIcon} />
+                    <input className={classes.dashboardSearchBox}  placeholder="Search Destination..." />
+                </Box>
+            </Box>
+            <Box className={classes.dahsboardNotification}>
+                <img src={BellIcon} alt='icon' />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
--- src/components/Dashboard/stylesheet.js
+++ src/components/Dashboard/stylesheet.js
@@ -6,7 +6,68 @@ export const useStyles = makeStyles({
     background: "#F5F9F8",
     height: "95vh",
     marginTop: "22px",
-    borderRadius: '35px',
-    marginLeft: '16.5rem'
-  }
+    borderRadius: "35px",
+    marginLeft: "16.5rem",
+  },
+  dashboardInnerContainer: {
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+    paddingTop: "1rem",
+    paddingBottom: "1.5rem",
+  },
+  dashboardUserStuffs: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  dashboardUserGreet: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  dahsboardUserGreeting: {
+    fontSize: "28px",
+    fontWeight: 700,
+    lineHeight: "41.02px",
+    letterSpacing: "2%",
+    paddingBottom: "4px",
+  },
+  dashboardWelcomeGreeting: {
+    fontSize: "14px",
+    fontWeight: 400,
+    lineHeight: "21.09px",
+    letterSpacing: "2%",
+    color: "#aaa",
+  },
+  dashboardSearchItems: {
+      display: 'flex',
+      justifyContent: 'space-between'
+  },
+  dashboardDestinationSearchBox: {
+    width: "280px",
+    height: "42px",
+    borderRadius: "20px",
+    boxShadow: "0px 3px 10px 0px rgba(0, 0, 0, 0.09)",
+    backgroundColor: "#fff",
+    display: "flex",
+    alignItems: "center",
+    marginRight: '15px'
+  },
+  searchIcon: {
+    marginLeft: "10px",
+    fill: "#aaa",
+  },
+  dashboardSearchBox: {
+    marginLeft: "15px",
+    border: 'none',
+    outline: 'none'
+  },
+  dahsboardNotification: {
+    width: "42px",
+    height: "42px",
+    borderRadius: "50%",
+    boxShadow: "0px 3px 10px 0px rgba(0, 0, 0, 0.09)",
+    backgroundColor: '#fff',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 });
